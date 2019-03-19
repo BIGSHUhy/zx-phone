564132ff4c338f61f07b2e90d1c135c6250bbaa4
shuhy <shuhy@honpresent.com>
2019-03-19 18:48:53 +0800

    头部完成

--- js/index.js
+++ js/index.js
@@ -1,8 +1,29 @@
 
 // 轮播图
-var mySwiper = new Swiper('.swiper-container',{
-	autoplay: 1000,
-	noSwiping : true,
-	pagination : '.swiper-pagination',
+var mySwiper = new Swiper('.banner-container',{
+	
+	
 	loop : true,
+	pagination: {
+		el: '.swiper-pagination',
+	  },
+	autoplay: {
+		delay: 3000,
+		stopOnLastSlide: false,
+		disableOnInteraction: true,
+		},
+		
 	})
+
+
+	
+    var swiper = new Swiper('.headlines-container', {
+		direction: 'vertical',
+		loop : true,
+		autoplay: {
+			delay: 1000,
+			stopOnLastSlide: false,
+			disableOnInteraction: true,
+			},
+	  });
+	
