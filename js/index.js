7007b4107ea05ff7146984c9490d35103782a621
shuhy <shuhy@honpresent.com>
2019-03-21 18:44:40 +0800

    移动端优化

--- js/index.js
+++ js/index.js
@@ -1,3 +1,19 @@
+// 监听滚动条
+$(document).scroll(function() {
+	// console.log($(this).scrollTop())
+	if($(this).scrollTop() > 69) {
+		$(".header-box").css({"margin-bottom":"0"})
+		$(".banner-container").css({"z-index":"998"})
+	} else {
+		$(".header-box").css({"margin-bottom":"8rem"})
+		$(".banner-container").css({"z-index":"1000"})
+	}
+});
+
+
+
+
+
 
 // 轮播图
 var mySwiper = new Swiper('.banner-container',{
