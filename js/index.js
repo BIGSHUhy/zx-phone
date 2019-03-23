d41e0ab12491d1243bd7a39e6f37fee668def91d
shuhy <shuhy@honpresent.com>
2019-03-23 18:41:52 +0800

    3.23修改BUG

--- js/index.js
+++ js/index.js
@@ -1,6 +1,17 @@
 // 监听滚动条
 $(document).scroll(function() {
 	// console.log($(this).scrollTop())
+	// if($(this).scrollTop() > 69) {
+	// 	$(".header-box").css({"margin-bottom":"0"})
+	// 	$(".banner-container").css({"z-index":"998"})
+	// } else {
+	// 	$(".header-box").css({"margin-bottom":"8rem"})
+	// 	$(".banner-container").css({"z-index":"1000"})
+	// }
+});
+$('body').bind('touchmove', function(e) { 
+	e.preventdefault();               //禁用默认滚动行为，需要自己实现滚动
+	// console.log($(this).scrollTop()); // 计算你的屏幕高度
 	if($(this).scrollTop() > 69) {
 		$(".header-box").css({"margin-bottom":"0"})
 		$(".banner-container").css({"z-index":"998"})
@@ -9,6 +20,16 @@ $(document).scroll(function() {
 		$(".banner-container").css({"z-index":"1000"})
 	}
 });
+// window.addEventListener('scroll', function () {
+// 	// console.log($(this).scrollTop())
+// 	if($(this).scrollTop() > 69) {
+// 		$(".header-box").css({"margin-bottom":"0"})
+// 		$(".banner-container").css({"z-index":"998"})
+// 	} else {
+// 		$(".header-box").css({"margin-bottom":"8rem"})
+// 		$(".banner-container").css({"z-index":"1000"})
+// 	}
+// })
 
 
 
